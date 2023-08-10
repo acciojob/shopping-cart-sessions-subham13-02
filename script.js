65ef37d92ab315a9ceb43f9d79f4fc4bf7129fda
Subham Sahu <115224258+subham13-02@users.noreply.github.com>
2023-08-10 12:05:44 +0530

    created a submission

--- script.js
+++ script.js
@@ -11,6 +11,11 @@ const products = [
 
 // DOM elements
 const productList = document.getElementById("product-list");
+const cartList = document.getElementById("cart-list");
+const clearCartBtn = document.getElementById("clear-cart-btn");
+
+// Cart data
+const cartData = JSON.parse(sessionStorage.getItem("cart")) || [];
 
 // Render product list
 function renderProducts() {
@@ -22,16 +27,57 @@ function renderProducts() {
 }
 
 // Render cart list
-function renderCart() {}
+function renderCart() {
+  cartList.innerHTML = "";
+  let total = 0;
+
+  cartData.forEach((item) => {
+    const cartItem = document.createElement("li");
+    cartItem.textContent = `${item.name} - $${item.price}`;
+    cartList.appendChild(cartItem);
+    total += item.price;
+  });
+
+  const totalItem = document.createElement("li");
+  totalItem.textContent = `Total: $${total}`;
+  cartList.appendChild(totalItem);
+}
 
 // Add item to cart
-function addToCart(productId) {}
+function addToCart(productId) {
+  const productToAdd = products.find((product) => product.id === productId);
+  cartData.push(productToAdd);
+  sessionStorage.setItem("cart", JSON.stringify(cartData));
+  renderCart();
+}
 
 // Remove item from cart
-function removeFromCart(productId) {}
+function removeFromCart(productId) {
+  const index = cartData.findIndex((item) => item.id === productId);
+  if (index !== -1) {
+    cartData.splice(index, 1);
+    sessionStorage.setItem("cart", JSON.stringify(cartData));
+    renderCart();
+  }
+}
 
 // Clear cart
-function clearCart() {}
+function clearCart() {
+  cartData.length = 0;
+  sessionStorage.removeItem("cart");
+  renderCart();
+}
+
+// Add event listener for product list
+productList.addEventListener("click", (event) => {
+  if (event.target.classList.contains("add-to-cart-btn")) {
+    const productId = parseInt(event.target.dataset.id);
+    addToCart(productId);
+  }
+});
+
+// Add event listener for clear cart button
+clearCartBtn.addEventListener("click", clearCart);
 
 // Initial render
 renderProducts();
